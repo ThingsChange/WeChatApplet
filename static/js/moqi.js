6be2e6491ea717b30e0228beed02d4ef7ca97541
admin <959459496@qq.com>
2017-05-25 17:27:38 +0800

    地图可点击 区域添加 颜色提醒

--- static/js/moqi.js
+++ static/js/moqi.js
@@ -597,7 +597,7 @@ require(['jquery','migrate','template','chart','charts','jbox','progressBar','co
                     innerBoxArr.push(arr);
                     leftFlag = 0;
                 }
-            }
+            } 
             window.timeOut = setInterval(setLeft.bind(null, innerBoxArr), 10);
         }
 
@@ -696,7 +696,7 @@ require(['jquery','migrate','template','chart','charts','jbox','progressBar','co
             "dis_h": 200, //
             "$cheangeMap": $("#changeMap"), //进入地图按钮
             "inColor":"#1d4b99",  //地图选中区域颜色
-            "outColor":"#1c2d49",  //地图默认颜色 
+            "outColor":"#1b2769",  //地图默认颜色 
             
             "init": function() {
                 mapApi.getMap($("#moqi"));
